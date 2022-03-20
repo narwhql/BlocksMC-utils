0f3ebc3bbf2117408c7cb9984b1fe713794b3935
narwhql <roniemam2005@gmail.com>
2022-03-20 19:00:52 +0100

    Added new methods

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ module.exports.fetchPlayer = require('./core/players/players-core');
 module.exports.fetchLeaderboards = require('./core/lb/leaderboards-core');
 module.exports.fetchGuild = require('./core/guilds/guilds-core');
 module.exports.fetchChatReport = require('./core/chatreports/chatreports-core');
+module.exports.staff = require('./core/staff/staff-core');
+module.exports.fetchTopJoin = require('./core/topjoin/topjoin-core');
+module.exports.fetchTopDonators = require('./core/topdonators/topdonators-core');
