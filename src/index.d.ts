d23920e659c02b732ec77dbdc2d45c9799f1d873
narwhql <roniemam2005@gmail.com>
2022-03-20 19:00:45 +0100

    Added new methods

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,8 +3,14 @@ import { LeaderboardGameType } from "./types";
 import { Leaderboard } from "./core/lb/Leaderboards";
 import { Guild } from "./core/guilds/Guilds";
 import { ChatReport } from "./core/chatreports/ChatReports";
+import { Staff } from "./core/staff/Staff";
+import { TopJoinPlayer } from "./core/topjoin/TopJoin";
+import { TopDonatorPlayer } from "./core/topdonators/TopDonators";
 
 export function fetchPlayer(username: string): Promise<Player | void>;
 export function fetchLeaderboards(game: LeaderboardGameType): Promise<Leaderboard | void>;
 export function fetchGuild(name: string): Promise<Guild | void>;
 export function fetchChatReport(code: string): Promise<ChatReport | void>;
+export const staff: Staff;
+export function fetchTopJoin(): Promise<TopJoinPlayer[]>;
+export function fetchTopDonators(): Promise<TopDonatorPlayer[]>;
